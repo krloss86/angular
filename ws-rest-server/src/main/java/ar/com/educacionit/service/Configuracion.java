5abc807deeb726d83a6909ffe467d280a5feab59
krloss@gmail.com <krloss@gmail.com>
2020-01-30 13:24:03 -0300

    delete app-rest

--- ws-rest-server/src/main/java/ar/com/educacionit/service/Configuracion.java
+++
@@ -1,9 +0,0 @@
-package ar.com.educacionit.service;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("api")
-public class Configuracion extends Application {
-
-}
